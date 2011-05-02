7b8b39ebbfed2558fdb2f6d91fd2874ea8c8acc0
Sebastian Koch <skoch@gris.tu-darmstadt.de>
2011-05-02 22:12:31 +0200

    ue 2 matlab code a little bit nicer

--- assignment2/ue2.m
+++ assignment2/ue2.m
@@ -1,82 +1,91 @@
 clear;
 close all;
-
+beep off;
 load usps_all.mat;
 
 avg = mean(double(data));
 
 
 
-%plot averages as histograms:
+'plotting averages of features as histograms:'
 
-% bins = linspace(10,190,60)
-% 
-% subplot(2,1,1);
-% hist(avg(:,:,1), bins);
-% h = findobj(gca,'Type','patch');
-% set(h,'FaceColor', 'b');
-% 
-% subplot(2,1,2);
-% hist(avg(:,:,8), bins);
-% h = findobj(gca,'Type','patch');
-% set(h,'FaceColor', 'r');
-% 
-% figure;
-% 
-% subplot(2,1,1);
-% hist(avg(:,:,2), bins);
-% h = findobj(gca,'Type','patch');
-% set(h,'FaceColor', 'b');
-% 
-% subplot(2,1,2);
-% hist(avg(:,:,5), bins);
-% h = findobj(gca,'Type','patch');
-% set(h,'FaceColor', 'r');
+ bins = linspace(10,190,60);
+
+ figure('Name', '1 vs 8');
+subplot(2,1,1);
+hist(avg(:,:,1), bins);
+h = findobj(gca,'Type','patch');
+set(h,'FaceColor', 'b');
+
+subplot(2,1,2);
+hist(avg(:,:,8), bins);
+h = findobj(gca,'Type','patch');
+set(h,'FaceColor', 'r');
+
+figure('Name', '2 vs 5');
+
+subplot(2,1,1);
+hist(avg(:,:,2), bins);
+h = findobj(gca,'Type','patch');
+set(h,'FaceColor', 'b');
+
+subplot(2,1,2);
+hist(avg(:,:,5), bins);
+h = findobj(gca,'Type','patch');
+set(h,'FaceColor', 'r');
+
+input('');
 
 
 %fit gaussian distributions from training data only
 
+%split data into training data and test data
 train_data = data(:,1:550,:);
+test_data = data(:,551:end,:);
+
 train_avg = mean(double(train_data));
 
-
+% compute mean and variance for all digits (this is the maximum
+% likelihood fit of a gaussian to our training data)
 mean_train = mean(train_avg(:,:,:));
 mean_train_mat = repmat(mean_train, 1, 550);
-
-
 var = sum( (train_avg(:,:,:)-mean_train_mat).^2 )./ size(train_data,2);
 
 
-test_data = data(:,551:end,:);
+% test feature on all test data
 test_avg = mean(double(test_data));
 
 post = [normpdf(test_avg(:), mean_train(1,1,1), sqrt(var(1)))*0.5, ... 
         normpdf(test_avg(:), mean_train(1,1,8), sqrt(var(8)))*0.5];
 
-%plot posterior for test data
+'plotting posterior for all test data:'
 
-% subplot(2,1,1);
-% plot(post(:,1));
-% subplot(2,1,2);
-% plot(post(:,2));
+ subplot(2,1,1);
+ plot(post(:,1));
+ subplot(2,1,2);
+ plot(post(:,2));
 
+ input('');
+
+ % test feature on 1 vs 8
 test_small = [data(:,551:end,1),data(:,551:end,8)];
 test_avg = mean(double(test_small));
 
 post = [normpdf(test_avg(:), mean_train(1,1,1), sqrt(var(1)))*0.5, ... 
         normpdf(test_avg(:), mean_train(1,1,8), sqrt(var(8)))*0.5];
 
+'plotting posterior for 1 and 8 only:'
 
-%plot posterior for 1 and 8 images only
-
-% subplot(2,1,1);
-% plot(post(:,1))
-% subplot(2,1,2);
-% plot(post(:,2))
+ subplot(2,1,1);
+ plot(post(:,1))
+ subplot(2,1,2);
+ plot(post(:,2))
 
 [Y,I]= max(post, [], 2);
 
-% error rate for 1 and 8
+input('')
+
+'error rate for 1 versus 8'
 1.0-(sum(I(1:550)==1)+sum(I(551:end)==2))/size(I,1)
 
 
@@ -86,21 +95,10 @@ post = [normpdf(test_avg(:), mean_train(1,1,2), sqrt(var(2)))*0.5, ...
         normpdf(test_avg(:), mean_train(1,1,5), sqrt(var(5)))*0.5];
 
 [Y,I]= max(post, [], 2);
-% error rate for 2 and 5
+'error rate for 2 and 5'
 1.0-(sum(I(1:550)==1)+sum(I(551:end)==2))/size(I,1)
 
-
-% x = linspace(0,255, 255);
-% 
-% colors = ['r.', 'g.', 'b.', 'c.','y.','m.','k.'];
-% 
-% 
-% for i=1:7
-%     colors((i-1)*2+1:i*2)
-%     plot(normpdf(x, mean_train(1,1,i), sqrt(var(i))),  colors((i-1)*2+1:i*2));
-%     hold on;
-% end
-
+'check if posterior sums to 1'
 
 x = rand*255;
 
@@ -140,7 +138,7 @@ post = [normpdf(test_avg(:), mean_train(1,1,1), sqrt(var(1)))*0.1, ...
     
 [Y,I]= max(post, [], 2);
 
-% error rate for multi class predictions
+'error rate for multi class predictions:'
 1.0-(sum(I(1:550)==1) ...
     +sum(I(550*1+1:550*2)==2) ...
     +sum(I(550*2+1:550*3)==3) ...
@@ -156,6 +154,7 @@ post = [normpdf(test_avg(:), mean_train(1,1,1), sqrt(var(1)))*0.1, ...
 
 % which is a little bit better than chance
 
+'confusion matrix for multi class predictions:'
 M = zeros(10,10);
 
 for i=1:10
@@ -166,8 +165,11 @@ for i=1:10
               sum(I(550*(i-1)+1:550*i)==9), sum(I(550*(i-1)+1:550*i)==10)];
 end
 
-M;
+M
 
+% compute the mean of the approximate of the derivative as a feature
+% (thanks to Ronny for the idea, our template based feature did not really
+% work that well)
 
 train_response = mean(diff(data(:,1:550,:)));
 
@@ -176,11 +178,6 @@ mean_train_mat = repmat(mean_train, 1, 550);
 
 var = sum( (train_response(:,:,:)-mean_train_mat).^2 )./ size(train_data,2);
 
-% x = linspace(0,100, 3000);
- 
-% plot(normpdf(x, mean_train(1,1,1), sqrt(var(1))),  'r.');
-% hold on;
-% plot(normpdf(x, mean_train(1,1,8), sqrt(var(8))),  'g.');
 
 test_small = [data(:,551:end,1),data(:,551:end,8)];
 test_avg = mean(diff(test_small));
@@ -190,7 +187,6 @@ post = [normpdf(test_avg(:), mean_train(1,1,1), sqrt(var(1)))*0.5, ...
 
 [Y,I]= max(post, [], 2);
 
-% error rate for 1 and 8 with new feature
 'error rate for new feature to distinguish between  1 and 8'
 1.0-(sum(I(1:550)==1)+sum(I(551:end)==2))/size(I,1)
 
@@ -210,12 +206,15 @@ post = [normpdf(test_avg(:), mean_train(1,1,2), sqrt(var(2)))*0.5, ...
 
 
 
+
+% compute the new and old features for training data
 feature_new = mean(diff(data(:,1:550,:)));
 feature_old = mean(data(:,1:550,:));
 
-
+% concatenate them
 feature = [feature_new; feature_old];
 
+% compute the mean and the covariance (maximum likelihood)
 mean_feature = mean(feature(:,:,:),2);
 
 covariances = zeros(2,2,10);
@@ -233,18 +232,8 @@ post = [mvnpdf(test_feature', mean_feature(:,1,1)', covariances(:,:,1)).*0.5, ..
 
 [Y,I]= max(post, [], 2);
 
-% error rate for 1 and 8 with new feature
-'error rate for new feature to distinguish between  1 and 8'
+'error rate for combined feature to distinguish between  1 and 8'
 1.0-(sum(I(1:550)==1)+sum(I(551:end)==2))/size(I,1)
-
-%x = rand(10000,2).*repmat([40, 200],10000,1) ;
-
-%one = mvnpdf(x, mean_feature(:,1,1)', covariances(:,:,1)).*0.5;
-%eight = mvnpdf(x, mean_feature(:,1,8)', covariances(:,:,8)).*0.5;
-
-%plot3(x(:,1), x(:,2),one(:), 'r.');
-%hold on;
-%plot3(x(:,1), x(:,2),eight(:), 'b.');
 
 
 test_small = [data(:,551:end,2),data(:,551:end,5)];
@@ -255,16 +244,6 @@ post = [mvnpdf(test_feature', mean_feature(:,1,2)', covariances(:,:,2)).*0.5, ..
 
 [Y,I]= max(post, [], 2);
 
-% error rate for 1 and 8 with new feature
-'error rate for new feature to distinguish between  2 and 5'
+'error rate for combined feature to distinguish between  2 and 5'
 1.0-(sum(I(1:550)==1)+sum(I(551:end)==2))/size(I,1)
-  
 
-%x = rand(10000,2).*repmat([40, 200],10000,1) ;
-
-%two = mvnpdf(x, mean_feature(:,1,2)', covariances(:,:,2)).*0.5;
-%five = mvnpdf(x, mean_feature(:,1,5)', covariances(:,:,5)).*0.5;
-
-%plot3(x(:,1), x(:,2),two(:), 'r.');
-%hold on;
-%plot3(x(:,1), x(:,2),five(:), 'b.');
